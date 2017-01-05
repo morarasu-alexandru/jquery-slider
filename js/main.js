e57feb0665deba66e0d7e0a97e0696f4486a0eba
Alexandru Morarasu <morarasu_alexandru@yahoo.com>
2017-01-05 19:42:07 +0200

    work in progress

--- js/main.js
+++ js/main.js
@@ -4,19 +4,26 @@ var slider = $('.slider');
 var sliderItem = $('.sliderItem');
 var slidercount = sliderItem.length;
 var currentSlide = 1;
+var runSlide = false;
+var playButton = document.getElementById('playButton');
 
 function slide(){
-    slider.animate({'margin-left': '-='+width}, 1000, function () {
-        if (++currentSlide === slidercount) {
+        slider.animate({'margin-left': '-='+width}, 1000, function () {
+            if (++currentSlide === slidercount) {
                 currentSlide = 1;
                 slider.css('margin-left', 0);
-        }
-    });
+            }
+        });
 }
 
 function runSlider() {
-    setInterval(slide, 3000);
+    runSlide = !runSlide;
+    if (runSlide) {
+        setInterval(slide, 3000);
+    } else {
+        slider.css('margin-left', 0);
+    }
+    console.log(runSlide);
 }
 
-runSlider();
-
+playButton.addEventListener('click', runSlider);
